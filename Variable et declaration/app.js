91758a70d693bea7a54f97c117bd325d6a92f912
miranto117 <tojo.mirantoratsimbazafy@gmail.com>
2024-02-23 22:27:47 +0100

    base variale et declaration

---
+++ Variable et declaration/app.js
@@ -0,0 +1,53 @@
+//déclaration
+let message;
+// Affectation
+message = 'Hello';
+console.log(message)
+
+// Var peut etre redéclarer
+var variable = "Bonjour";
+var variable = "Bonjour toi"
+console.log(variable)
+
+// Erreur de redeclaration
+// let message;
+
+//Limitaion nom de variable
+// Il peut être déclarer que avec des lettres, nombre, symbole $ et _
+let $ = 1, _ = 2;
+console.log($ + _);
+
+// le nommbre ne doit pas se declarer en premier
+// let 1variable = "testons";
+
+//autre symbole 
+// let variable! = "testons"
+
+// les traits d'union ne sont pas autorisé
+// let nom-variable = "teston"
+
+// Respecter la casse 
+let casse = "casse"
+console.log(casse)
+let CASSE = "casse"
+console.log(CASSE)
+
+// declaration et affection sans use strict
+num = 7
+console.log(num)
+
+//une constante est une variable inchangable
+const myBrithday = '01.01.1789'
+console.log(myBrithday);
+
+
+//Exercice
+let admin, name;
+name = "John";
+admin = name;
+console.log(admin);
+
+//Exercice sur les nomemclature
+let notrePlanete  = "Terre"
+let currentUserName = Null;
+
